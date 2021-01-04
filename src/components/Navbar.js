a519cac15e2281ad56e58cc5d85c8c742c1e47d3
Ashley <teshay93@hotmail.com>
2021-01-04 18:07:05 -0500

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+height: 60px;
+background: red;
+`;
+
+const Navbar = () => {
+    return (
+        <Nav>
+            <h1>Navbar</h1>
+        </Nav>
+    )
+}
+
+export default Navbar
